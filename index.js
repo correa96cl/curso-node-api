70d2f9e07616780567ca2e7caace74e491ee2fbd
Marcelo Valderrama Correa <marcelo.correa@mjv.com.br>
2020-06-12 22:02:56 -0300

    Aula Nova

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var express = require('express');
 
- var app = express()
+ var app = require('./config/custom-express')();
 
 app.listen(3000, function(){
     console.log('O server esta rodando na porta 3000');
 
 });
 
-app.get('/teste', function(req, res){
-    res.send('OK');
-
-});
